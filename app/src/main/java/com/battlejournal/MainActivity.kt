8fabc7908dbc710ce5296f80a06164483502f610
Colin Miller <jearil@fb.com>
2019-01-24 17:22:54 -0800

    Added FB and Google Login

--- app/src/main/java/com/battlejournal/MainActivity.kt
+++ app/src/main/java/com/battlejournal/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.battlejournal
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    val RC_SIGN_IN = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showLogin()
+
+        val logout = findViewById<Button>(R.id.logout_button)
+        logout.setOnClickListener {
+            AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener {
+                    showLogin()
+                }
+        }
+    }
+
+    private fun showLogin() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder()
+                .setPermissions(arrayListOf("email", "public_profile")).build())
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(), RC_SIGN_IN
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            // val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                //val user = FirebaseAuth.getInstance().currentUser
+                // do the thing
+                Toast.makeText(this, "Sign-in worked", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Sign-in failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
